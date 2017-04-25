00d1c413d31c5811e2b06aae4acf9ec560b95873
Matt Dieselman <dieselmanm@gmail.com>
2017-04-25 04:47:05 -0400

    Added Particle Systems and added 2 in the game (fire and white smoke).

--- DX11Starter/RenderManager.cpp
+++ DX11Starter/RenderManager.cpp
@@ -57,9 +57,36 @@ void RenderManager::setObjData(Entity * object)
 	object->getMat()->getPixelShader()->CopyAllBufferData();
 }
 
+SimpleVertexShader * RenderManager::getPartVert()
+{
+	return partVertexShader;
+}
+
+SimplePixelShader * RenderManager::getPartPix()
+{
+	return partPixelShader;
+}
+
+ID3D11ShaderResourceView* RenderManager::getPartText(int index)
+{
+	return particleTextures[index];
+}
+
 
 void RenderManager::LoadShaders(ID3D11Device* device, ID3D11DeviceContext* context)
 {
+
+	//Particle Shader Loading
+	partVertexShader = new SimpleVertexShader(device, context);
+	partPixelShader = new SimplePixelShader(device, context);
+
+
+	if (!partVertexShader->LoadShaderFile(L"Debug/PartVertexShader.cso"))
+		partVertexShader->LoadShaderFile(L"PartVertexShader.hlsl");
+
+	if (!partPixelShader->LoadShaderFile(L"Debug/PartPixelShader.cso"))
+		partPixelShader->LoadShaderFile(L"PartPixelShader.hlsl");
+
 	SimpleVertexShader* vertexShader = new SimpleVertexShader(device, context);
 	if (!vertexShader->LoadShaderFile(L"Debug/VertexShader.cso"))
 		vertexShader->LoadShaderFile(L"VertexShader.hlsl");
@@ -75,7 +102,16 @@ void RenderManager::LoadShaders(ID3D11Device* device, ID3D11DeviceContext* conte
 	ID3D11ShaderResourceView* texture3;
 	CreateWICTextureFromFile(device, context, L"textures/disc.png", 0, &texture3);
 	ID3D11ShaderResourceView* texture4;
-	CreateWICTextureFromFile(device, context, L"textures/rock.jpg", 0, &texture4);
+	CreateWICTextureFromFile(device, context, L"textures/rock.jpg", 0, &texture4);;
+
+	ID3D11ShaderResourceView* particleTexture;
+	ID3D11ShaderResourceView* particleTexture2;
+
+	CreateWICTextureFromFile(device, context, L"textures/fireParticle.jpg", 0, &particleTexture);
+	CreateWICTextureFromFile(device, context, L"textures/particle.jpg", 0, &particleTexture2);
+
+	particleTextures.push_back(particleTexture);
+	particleTextures.push_back(particleTexture2);
 
 	textures.push_back(texture1);
 	textures.push_back(texture2);
@@ -98,6 +134,31 @@ void RenderManager::LoadShaders(ID3D11Device* device, ID3D11DeviceContext* conte
 
 	device->CreateSamplerState(&sampDesc, &sampler);
 
+	//Particle depth and blend states
+	D3D11_DEPTH_STENCIL_DESC dsDesc = {};
+	dsDesc.DepthEnable = true;
+	dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ZERO;
+	dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
+	device->CreateDepthStencilState(&dsDesc, &particleDepthState);
+
+	D3D11_BLEND_DESC blend = {};
+	blend.AlphaToCoverageEnable = false;
+	blend.IndependentBlendEnable = false;
+	blend.RenderTarget[0].BlendEnable = true;
+	blend.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+	blend.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+	blend.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+	blend.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+	device->CreateBlendState(&blend, &particleBlendState);
+
+
+	//Create Emitter for parts
+
+	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
 
 	materials.push_back(new Material(vertexShader, pixelShader));
 	materials[0]->AttatchTexture(textures[0], sampler);
@@ -116,8 +177,9 @@ void RenderManager::LoadShaders(ID3D11Device* device, ID3D11DeviceContext* conte
 	materials[3]->AttatchNormalMap(normalMaps[0]);
 }
 
-void RenderManager::DrawAll(ID3D11DeviceContext * context, float deltaTime, float totalTime, std::vector<Entity*> gameObjects, Camera * cam, ID3D11RenderTargetView* backBufferRTV,ID3D11DepthStencilView* depthStencilView)
+void RenderManager::DrawAll(ID3D11DeviceContext * context, float deltaTime, float totalTime, std::vector<Entity*> gameObjects, Camera * cam, std::vector<Emitter*> emitters, ID3D11RenderTargetView* backBufferRTV,ID3D11DepthStencilView* depthStencilView)
 {
+
 	for each (Entity* object in gameObjects)
 	{
 		setObjData(object);
@@ -162,4 +224,18 @@ void RenderManager::DrawAll(ID3D11DeviceContext * context, float deltaTime, floa
 			0,     // Offset to the first index we want to use
 			0);    // Offset to add to each index when looking up vertices
 	}
+
+	//Draw Emitter & parts
+
+
+	float blend[4] = { 1,1,1,1 };
+	context->OMSetBlendState(particleBlendState, blend, 0xffffffff);  // Additive blending
+	context->OMSetDepthStencilState(particleDepthState, 0);			// No depth WRITING
+	for each (Emitter * e in emitters)
+		e->DrawAll(context, cam);
+
+	//reset drawstates
+	context->OMSetBlendState(0, blend, 0xffffffff);
+	context->OMSetDepthStencilState(0, 0);
+	
 }
--- DX11Starter/RenderManager.h
+++ DX11Starter/RenderManager.h
@@ -7,6 +7,7 @@
 #include "PointLight.h"
 #include "SpotLight.h"
 #include <WICTextureLoader.h>
+#include "Emitter.h"
 
 class RenderManager{
 
@@ -14,6 +15,14 @@ private:
 	std::vector<ID3D11ShaderResourceView*> textures;
 	std::vector<ID3D11ShaderResourceView*> normalMaps;
 	std::vector<Material*> materials;
+	std::vector<ID3D11ShaderResourceView*>particleTextures;
+
+
+	SimpleVertexShader* partVertexShader;
+	SimplePixelShader* partPixelShader;
+	ID3D11DepthStencilState* particleDepthState;
+	ID3D11BlendState* particleBlendState;
+
 	//std::vector<DirectionalLight> lights;
 
 public:
@@ -21,12 +30,17 @@ public:
 	~RenderManager();
 
 	std::vector<Material*> getMaterials();
-
 	void setSceneData(Camera* cam, DirectionalLight dirLight, PointLight pointLight, SpotLight spotLight);
 	void setObjData(Entity* object);
 
+	SimpleVertexShader * getPartVert();
+	SimplePixelShader * getPartPix();
+	ID3D11ShaderResourceView* getPartText(int index);
+
+
 	void LoadShaders(ID3D11Device* device, ID3D11DeviceContext* context);
-	void DrawAll(ID3D11DeviceContext* context, float deltaTime, float totalTime, std::vector<Entity*> gameObjects,Camera * cam, ID3D11RenderTargetView* backBufferRTV, ID3D11DepthStencilView* depthStencilView);
+	void DrawAll(ID3D11DeviceContext* context, float deltaTime, float totalTime, std::vector<Entity*> gameObjects,Camera * cam,std::vector<Emitter*> emitters, ID3D11RenderTargetView* backBufferRTV, ID3D11DepthStencilView* depthStencilView);
 	
+
 };
 
